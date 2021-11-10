ac338ea47c8b72f9567a5d84a9fb1f9436875260
kelpabc123 <71921094+kelpabc123@users.noreply.github.com>
2021-11-10 00:35:54 -0800

    Dexed RL compatibility

---
+++ audio.py
@@ -0,0 +1,283 @@
+"""
+Audio utils (spectrograms, G&L phase reconstruction, ...)
+"""
+
+import os
+import warnings
+from typing import Iterable, Sequence, Optional
+import pathlib
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.fft
+import librosa
+import librosa.display
+import soundfile as sf
+
+
+class Spectrogram:
+    """ Class for dB spectrogram computation from a raw audio waveform.
+    The min spectrogram value must be provided.
+    The default windowing function is Hann. """
+    def __init__(self, n_fft, fft_hop, min_dB, dynamic_range_dB=None, log_scale=True):
+        self.n_fft = n_fft
+        self.fft_hop = fft_hop
+        self.log_scale = log_scale
+        self.min_dB = min_dB
+        self.dynamic_range_dB = dynamic_range_dB
+        self.window = torch.hann_window(self.n_fft, periodic=False)
+        self.spectrogram_norm_factor = torch.fft.rfft(self.window).abs().max().item()
+
+    def get_stft(self, x_wav):
+        """ Returns the complex, non-normalized STFT computed from given audio. """
+        warnings.filterwarnings("ignore", category=UserWarning)  # Deprecation warning from PyTorch compiled-code
+        spectrogram = torch.stft(torch.tensor(x_wav, dtype=torch.float32), n_fft=self.n_fft, hop_length=self.fft_hop,
+                                 window=self.window, center=True,
+                                 pad_mode='constant', onesided=True, return_complex=True)
+        warnings.filterwarnings("default", category=UserWarning)
+        return spectrogram
+
+    def __call__(self, x_wav):
+        """ Returns the log-scale spectrogram of x_wav audio, with defined minimum 'floor' dB value. """
+        spectrogram = self.get_stft(x_wav).abs()
+        # normalization of spectrogram module vs. Hann window weight
+        spectrogram = spectrogram / self.spectrogram_norm_factor
+        if self.log_scale:
+            return self.linear_to_log_scale(spectrogram)
+        else:
+            return spectrogram
+
+    def linear_to_log_scale(self, spectrogram):
+        spectrogram = torch.maximum(spectrogram, torch.ones(spectrogram.size()) * 10 ** (self.min_dB / 20.0))
+        return 20.0 * torch.log10(spectrogram)
+
+    def log_to_linear_scale(self, spectrogram):
+        """ Reverses the log-scale applied to a Tensor spectrogram built by this class
+
+        :returns: the corresponding usual STFT-amplitude spectrogram """
+        stft = torch.pow(10.0, spectrogram/20.0)
+        return stft * self.spectrogram_norm_factor
+
+    def linear_to_log_scale_with_dynamic_range(self, spectrogram):
+        # TODO remove? Dynamic range might be a bad idea for VAEs... (need to reconstruct the 'floor' value)
+        assert self.dynamic_range_dB is not None  # Dynamic range not provided? It might be counterproductive anyway
+        spectrogram = torch.maximum(spectrogram, torch.ones(spectrogram.size()) * 10 ** (self.min_dB / 20.0))
+        spectrogram = 20.0 * torch.log10(spectrogram)
+        return torch.maximum(spectrogram,
+                             torch.ones(spectrogram.size()) * (torch.max(spectrogram) - self.dynamic_range_dB))
+
+
+
+class MelSpectrogram(Spectrogram):
+    def __init__(self, n_fft, fft_hop, min_dB, n_mel_bins, Fs):
+        super().__init__(n_fft, fft_hop, min_dB, log_scale=True)
+        # TODO add fmin, fmax arguments
+        self.Fs = Fs
+        self.n_mel_bins = n_mel_bins
+
+    def __call__(self, x_wav):
+        """ Returns a log-scale spectrogram with limited dynamic range """
+        spectrogram = self.get_stft(x_wav).abs()
+        spectrogram = spectrogram / self.spectrogram_norm_factor
+        # Torch-Numpy arrays share the same memory location (very fast convert)
+        spectrogram = librosa.feature.melspectrogram(S=spectrogram, n_mels=self.n_mel_bins,
+                                                     norm=None)  # for linear/mel specs magnitude compatibility
+        return self.linear_to_log_scale(torch.from_numpy(spectrogram))
+
+    def mel_dB_to_STFT(self, mel_spectrogram):
+        """ Inverses the Mel-filters and and log-amplitude transformations applied to a spectrogram. """
+        spectrogram = self.log_to_linear_scale(mel_spectrogram)
+        return librosa.feature.inverse.mel_to_stft(spectrogram.numpy(), n_fft=self.n_fft, power=1.0, norm=None)
+
+
+class SimilarityEvaluator:
+    """ Class for evaluating audio similarity between audio samples through various criteria. """
+    def __init__(self, x_wav: Sequence[Iterable], n_fft=1024, fft_hop=256, sr=22050, n_mfcc=13):
+        """
+
+        :param x_wav: List or Tuple which contains the 2 audio signals (arrays) to be compared.
+        :param n_fft:
+        :param fft_hop:
+        :param sr:
+        :param n_mfcc:
+        """
+        assert len(x_wav) == 2  # This class requires exactly 2 input audio signals
+        self.x_wav = x_wav
+        self.n_fft = n_fft
+        self.fft_hop = fft_hop
+        self.sr = sr
+        self.n_mfcc = n_mfcc
+        # Pre-compute STFT (used in mae log and spectral convergence)
+        self.stft = [np.abs(librosa.stft(x, self.n_fft, self.fft_hop)) for x in self.x_wav]
+
+    def get_mae_log_stft(self, return_spectrograms=True):
+        """ Returns the Mean Absolute Error on log(|STFT|) spectrograms of input sounds, and the two spectrograms
+        themselves (e.g. for plotting them later). """
+        eps = 1e-4  # -80dB  (un-normalized stfts)
+        log_stft = [np.maximum(s, eps) for s in self.stft]
+        log_stft = [np.log10(s) for s in log_stft]
+        mae = np.abs(log_stft[1] - log_stft[0]).mean()
+        return (mae, log_stft) if return_spectrograms else mae
+
+    def display_stft(self, s, log_scale=True):
+        """ Displays given spectrograms s (List of two |STFT|) """
+        fig, axes = plt.subplots(1, 2, figsize=(6, 3))
+        im = librosa.display.specshow(s[0], shading='flat', ax=axes[0], cmap='magma')
+        im = librosa.display.specshow(s[1], shading='flat', ax=axes[1], cmap='magma')
+        if log_scale:
+            axes[0].set(title='Reference $\log_{10} |STFT|$')
+        else:
+            axes[0].set(title='Reference $|STFT|$')
+        axes[1].set(title='Inferred synth parameters')
+        fig.tight_layout()
+        return fig, axes
+
+    def get_spectral_convergence(self, return_spectrograms=True):
+        """ Returns the Spectral Convergence of input sounds, and the two linear-scale spectrograms
+            used to compute SC (e.g. for plotting them later). SC: see https://arxiv.org/abs/1808.06719 """
+        # Frobenius norm is actually the default numpy matrix norm
+        # TODO check for 0.0 frob norm of stft[0]
+        sc = np.linalg.norm(self.stft[0] - self.stft[1], ord='fro') / np.linalg.norm(self.stft[0], ord='fro')
+        return (sc, self.stft) if return_spectrograms else sc
+
+    def get_mae_mfcc(self, return_mfccs=True, n_mfcc: Optional[int] = None):
+        """ Returns the Mean Absolute Error on MFCCs, and the MFCCs themselves.
+        Uses librosa default MFCCs configuration: TODO precise
+        """
+        mfcc = [librosa.feature.mfcc(x, sr=self.sr, n_mfcc=(self.n_mfcc if n_mfcc is None else n_mfcc))
+                for x in self.x_wav]
+        mae = np.abs(mfcc[0] - mfcc[1]).mean()
+        return (mae, mfcc) if return_mfccs else mae
+
+    def display_mfcc(self, mfcc):
+        fig, axes = plt.subplots(1, 2, figsize=(6, 3))
+        im = librosa.display.specshow(mfcc[0], shading='flat', ax=axes[0], cmap='viridis')
+        im = librosa.display.specshow(mfcc[1], shading='flat', ax=axes[1], cmap='viridis')
+        axes[0].set(title='{}-bands MFCCs'.format(self.n_mfcc))
+        axes[1].set(title='Inferred synth parameters')
+        fig.tight_layout()
+        return fig, axes
+
+
+
+
+class SimpleSampleLabeler:
+    def __init__(self, x_wav, Fs, hpss_margin=3.0, perc_duration_ms=250.0):
+        """ Class to attribute labels or a class to sounds, mostly based on librosa hpss and empirical thresholds.
+
+        :param x_wav:
+        :param Fs:
+        :param hpss_margin: see margin arg of librosa.decompose.hpss
+        :param perc_duration_ms: The duration of a percussion sound - most of the percussive energy should be found
+            before that time (in the percussive separated spectrogram).
+        """
+        assert Fs == 22050  # Librosa defaults must be used at the moment
+        self.x_wav = x_wav
+        self.Fs = Fs
+        self.hpss_margin = hpss_margin
+        self.perc_duration_ms = perc_duration_ms
+        # Pre-computation of spectrograms and energies
+        self.specs = self._get_hpr_specs()
+        self.energy, self.energy_ratio = self._get_energy_ratios()
+        # Energies on attack (to identify perc sounds)
+        # Perc content supposed to be found in the first 10s of ms. Hop: default librosa 256
+        limit_index = int(np.ceil(self.perc_duration_ms * self.Fs / 256.0 / 1000.0))
+        self.attack_specs = dict()
+        self.attack_energies = dict()
+        for k in self.specs:
+            self.attack_specs[k] = self.specs[k][:, 0:limit_index]  # indexes: f, t
+            self.attack_energies[k] = np.abs(self.attack_specs[k]).sum()
+        # Labels pre-computation... so it's done
+        self.is_harmonic = self._is_harmonic()
+        self.is_percussive = self._is_percussive()
+
+    def has_label(self, label):
+        if label == 'harmonic':
+            return self.is_harmonic
+        elif label == 'percussive':
+            return self.is_percussive
+        elif label == 'sfx':
+            return not self.is_harmonic and not self.is_percussive
+        else:
+            raise ValueError("Label '{}' is not valid.".format(label))
+
+    def _get_hpr_specs(self):
+        D = librosa.stft(self.x_wav)  # TODO custom fft params
+        H, P = librosa.decompose.hpss(D, margin=self.hpss_margin)
+        R = D - (H + P)
+        return {'D': D, 'H': H, 'P': P, 'R': R}
+
+    def _get_energy_ratios(self):
+        energy = dict()
+        for k in self.specs:
+            energy[k] = np.abs(self.specs[k]).sum()
+        return energy, {'D': 1.0, 'H': energy['H'] / energy['D'], 'P': energy['P'] / energy['D'],
+                        'R': energy['R'] / energy['D']}
+
+    def plot_hpr_specs(self, figsize=(8, 6)):
+        fig, axes = plt.subplots(4, 1, figsize=figsize)
+        axes = [axes]  # Unqueeze - to prepare for multi-cols display
+        for col in range(1):
+            im = librosa.display.specshow(librosa.amplitude_to_db(np.abs(self.specs['D']), ref=np.max), y_axis='log',
+                                          ax=axes[col][0])
+            fig.colorbar(im, format='%+2.0f dB', ax=axes[col][0])
+            axes[col][0].set(title='Full power spectrogram')
+            im = librosa.display.specshow(librosa.amplitude_to_db(np.abs(self.specs['H']), ref=np.max), y_axis='log',
+                                          ax=axes[col][1])
+            fig.colorbar(im, format='%+2.0f dB', ax=axes[col][1])
+            axes[col][1].set(title='Harmonic power spectrogram ({:.1f}% of total spectral power)'.format(
+                100.0 * self.energy_ratio['H']))
+            im = librosa.display.specshow(librosa.amplitude_to_db(np.abs(self.specs['P']), ref=np.max), y_axis='log',
+                                          ax=axes[col][2])
+            fig.colorbar(im, format='%+2.0f dB', ax=axes[col][2])
+            axes[col][2].set(title='Percussive power spectrogram ({:.1f}% of total spectral power)'.format(
+                100.0 * self.energy_ratio['P']))
+            im = librosa.display.specshow(librosa.amplitude_to_db(np.abs(self.specs['R']), ref=np.max), y_axis='log',
+                                          ax=axes[col][3])
+            fig.colorbar(im, format='%+2.0f dB', ax=axes[col][3])
+            axes[col][3].set(title='Residuals power spectrogram ({:.1f}% of total spectral power)'.format(
+                100.0 * self.energy_ratio['R']))
+        fig.tight_layout()
+        return fig, axes
+
+    def get_harmonic_sound(self):
+        return librosa.istft(self.specs['H'])
+
+    def get_percussive_sound(self):
+        return librosa.istft(self.specs['P'])
+
+    def get_residual_sound(self):
+        return librosa.istft(self.specs['R'])
+
+    def _is_harmonic(self):
+        if self.energy_ratio['H'] > 0.40:
+            return True
+        elif self.energy_ratio['H'] > 0.35:  # Harmonic with percussive attack
+            return (self.attack_energies['P'] / self.energy['P']) > 0.9
+        return False
+
+    def _is_percussive(self):
+        # Mostly percussive sound
+        if self.energy_ratio['P'] > 0.40:
+            return (self.attack_energies['P'] / self.energy['P']) > 0.9
+        # Percussive with harmonic attack
+        elif self.energy_ratio['P'] > 0.35 and self.energy_ratio['H'] > 0.15:
+            return (self.attack_energies['P'] / self.energy['P']) > 0.9\
+                   and (self.attack_energies['H'] / self.energy['H']) > 0.8
+        return False
+
+    def print_labels(self):
+        print("is_harmonic={}   is_percussive={}".format(self.is_harmonic, self.is_percussive))
+
+
+
+def write_wav_and_mp3(base_path: pathlib.Path, base_name: str, samples, sr):
+    """ Writes a .wav file and converts it to .mp3 using command-line ffmpeg (which must be available). """
+    wav_path_str = "{}".format(base_path.joinpath(base_name + '.wav'))
+    sf.write(wav_path_str, samples, sr)
+    mp3_path_str = "{}".format(base_path.joinpath(base_name + '.mp3'))
+    # mp3 320k will be limited to 160k for mono audio - still too much loss for HF content
+    os.system("ffmpeg -i {} -b:a 320k -y {}".format(wav_path_str, mp3_path_str))
+
---
+++ vae_config.py
@@ -0,0 +1,204 @@
+"""
+Allows easy modification of all configuration parameters required to define,
+train or evaluate a model.
+This script is not intended to be run, it only describes parameters.
+However, some dynamic hyper-parameters are properly set when this module is imported.
+
+This configuration is used when running train.py as main.
+When running train_queue.py, configuration changes are relative to this config.py file.
+
+When a run starts, this file is stored as a config.json file. To ensure easy restoration of
+parameters, please only use simple types such as string, ints, floats, tuples (no lists) and dicts.
+"""
+
+
+import datetime
+from utils.config import _Config  # Empty class - to ease JSON serialization of this file
+
+
+model = _Config()
+model.name = "FlVAE2"
+model.run_name = '03_debug'  # run: different hyperparams, optimizer, etc... for a given model
+model.allow_erase_run = True  # If True, a previous run with identical name will be erased before training
+# See model/encoder.py to view available architectures. Decoder architecture will be as symmetric as possible.
+model.encoder_architecture = 'speccnn8l1_bn'
+# Possible values: 'flow_realnvp_4l180', 'mlp_3l1024', ... (configurable numbers of layers and neurons)
+model.params_regression_architecture = 'flow_realnvp_6l300'
+model.params_reg_softmax = False  # Apply softmax in the flow itself? If False: cat loss can be BCE or CCE
+# Spectrogram size cannot easily be modified - all CNN decoders should be re-written
+model.note_duration = (3.0, 1.0) #1.0272
+model.extra_audio_samples = 600
+model.sampling_rate = 22050
+model.stft_args = (1024, 256)  # fft size and hop size
+model.mel_bins = 257  # -1 disables Mel-scale spectrogram. Try: 257, 513, ...
+model.mel_f_limits = (0, 11050)  # min/max Mel-spectrogram frequencies TODO implement
+# Tuple of (pitch, velocity) tuples. Using only 1 midi note is fine.
+model.midi_notes = ((60, 100), )  # Reference note
+# model.midi_notes = ((40, 85), (50, 85), (60, 42), (60, 85), (60, 127), (70, 85))
+model.stack_spectrograms = False  # If True, dataset will feed multi-channel spectrograms to the encoder
+model.stack_specs_deepest_features_mix = False  # if True, feats mixed in the deepest 1x1 conv, else in the deepest 4x4
+# If True, each preset is presented several times per epoch (nb of train epochs must be reduced) such that the
+# dataset size is artificially increased (6x bigger with 6 MIDI notes) -> warmup and patience epochs must be scaled
+model.increased_dataset_size = None  # See update_dynamic_config_params()
+model.spectrogram_min_dB = -120.0
+# Possible spectrogram sizes:
+#   (513, 433): audio 5.0s, fft size 1024, fft hop 256
+#   (257, 347): audio 4.0s, fft size 512 (or fft 1024 w/ mel_bins 257), fft hop 256
+model.spectrogram_size = (257, 347)  # see data/dataset.py to retrieve this from audio/stft params
+model.input_tensor_size = None  # see update_dynamic_config_params()
+# If True, encoder output is reduced by 2 for 1 MIDI pitch and 1 velocity to be concatenated to the latent vector
+model.concat_midi_to_z = None  # See update_dynamic_config_params()
+# Latent space dimension  *************** When using a Flow regressor, this dim is automatically set ******************
+model.dim_z = 256  # Including possibly concatenated midi pitch and velocity
+# Latent flow architecture, e.g. 'realnvp_4l200' (4 flows, 200 hidden features per flow)
+#    - base architectures can be realnvp, maf, ...
+#    - set to None to disable latent space flow transforms
+model.latent_flow_arch = 'realnvp_6l300'
+# If True, loss compares v_out and v_in. If False, we will flow-invert v_in to get loss in the q_Z0 domain.
+# This option has implications on the regression model itself (the flow will be used in direct or inverse order)
+model.forward_controls_loss = True  # Must be true for non-invertible MLP regression
+
+model.synth = 'dexed'
+# Dexed-specific auto rename: '*' in 'al*_op*_lab*' will be replaced by the actual algorithms, operators and labels
+model.synth_args_str = 'al*_op*_lab*'  # Auto-generated string (see end of script)
+model.synth_params_count = -1  # Will be set automatically - see data.build.get_full_and_split_datasets
+model.learnable_params_tensor_length = -1  # Will be set automatically - see data.build.get_full_and_split_datasets
+# Modeling of synth controls probability distributions
+# Possible values: None, 'vst_cat' or 'all<=xx' where xx is numerical params threshold cardinal
+model.synth_vst_params_learned_as_categorical = 'all<=32'
+# flags/values to describe the dataset to be used
+model.dataset_labels = None  # tuple of labels (e.g. ('harmonic', 'percussive')), or None to use all available labels
+# Dexed: Preset Algorithms, and activated Operators (Lists of ints, None to use all)
+# Limited algorithms (non-symmetrical only): [1, 2, 7, 8, 9, 14, 28, 3, 4, 11, 16, 18]
+# Other synth: ...?
+model.dataset_synth_args = (None, [1, 2, 3, 4, 5, 6])
+# Directory for saving metrics, samples, models, etc... see README.md
+model.logs_root_dir = "saved"  # Path from this directory
+
+
+train = _Config()
+train.start_datetime = datetime.datetime.now().isoformat()
+train.minibatch_size = 160
+train.main_cuda_device_idx = 1  # CUDA device for nonparallel operations (losses, ...)
+train.test_holdout_proportion = 0.2
+train.k_folds = 5
+train.current_k_fold = 0
+train.start_epoch = 0  # 0 means a restart (previous data erased). If > 0: will load start_epoch-1 checkpoint
+# Total number of epochs (including previous training epochs)
+train.n_epochs = 400  # See update_dynamic_config_params().  16k sample dataset: set to 700
+train.save_period = 50  # Period for checkpoint saves (large disk size). Tensorboard scalars/metric logs at all epochs.
+train.plot_period = 20  # Period (in epochs) for plotting graphs into Tensorboard (quite CPU and SSD expensive)
+train.latent_loss = 'Dkl'  # Latent regularization loss: Dkl or MMD for Basic VAE (Flow VAE has its own specific loss)
+# When using a latent flow z0-->zK, z0 is not regularized. To keep values around 0.0, batch-norm or a 0.1Dkl can be used
+train.latent_flow_input_regularization = 'bn'  # 'bn' (on encoder output) or 'dkl' (on q_Z0 gaussian flow input)
+train.params_cat_bceloss = False  # If True, disables the Categorical Cross-Entropy loss to compute BCE loss instead
+train.params_cat_softmax_temperature = 0.2  # Temperature if softmax if applied in the loss only
+# Losses normalization allow to get losses in the same order of magnitude, but does not optimize the true ELBO.
+# When un-normalized, the reconstruction loss (log-probability of a multivariate gaussian) is orders of magnitude
+# bigger than other losses. Train does not work with normalize=False at the moment - use train.beta to compensate
+train.normalize_losses = True  # Normalize all losses over the vector-dimension (e.g. spectrogram pixels count, D, ...)
+
+
+# TODO train regression network alone when full-train has finished?
+train.optimizer = 'Adam'
+# Maximal learning rate (reached after warmup, then reduced on plateaus)
+# LR decreased if non-normalized losses (which are expected to be 90,000 times bigger with a 257x347 spectrogram)
+train.initial_learning_rate = 2e-4  # e-9 LR with e+4 loss does not allow any train (vanishing grad?)
+# Learning rate warmup (see https://arxiv.org/abs/1706.02677)
+train.lr_warmup_epochs = 6  # See update_dynamic_config_params(). 16k samples dataset: set to 10
+train.lr_warmup_start_factor = 0.1
+train.adam_betas = (0.9, 0.999)  # default (0.9, 0.999)
+train.weight_decay = 1e-4  # Dynamic weight decay?
+train.fc_dropout = 0.3
+train.reg_fc_dropout = 0.4
+# (beta<1, normalize=True) corresponds to (beta>>1, normalize=False) in the beta-VAE formulation (ICLR 2017)
+train.beta = 0.2  # latent loss factor - use much lower value (e-2) to get closer the ELBO
+train.beta_start_value = 0.1  # Should not be zero (risk of a very unstable training)
+# Epochs of warmup increase from start_value to beta
+train.beta_warmup_epochs = 25  # See update_dynamic_config_params(). 16k samples dataset: set to 40
+train.beta_cycle_epochs = -1  # beta cyclic annealing (https://arxiv.org/abs/1903.10145). -1 deactivates TODO do
+
+train.scheduler_name = 'ReduceLROnPlateau'  # TODO try CosineAnnealing
+# Possible values: 'VAELoss' (total), 'ReconsLoss', 'Controls/BackpropLoss', ... All required losses will be summed
+train.scheduler_loss = ('ReconsLoss/Backprop', 'Controls/BackpropLoss')
+train.scheduler_lr_factor = 0.2
+# Set a longer patience with smaller datasets and quite unstable trains
+train.scheduler_patience = 6  # See update_dynamic_config_params(). 16k samples dataset:  set to 10
+train.scheduler_cooldown = 6  # See update_dynamic_config_params(). 16k samples dataset: set to 10
+train.scheduler_threshold = 1e-4
+# Training considered "dead" when dynamic LR reaches this value
+train.early_stop_lr_threshold = None  # See update_dynamic_config_params()
+
+train.verbosity = 1  # 0: no console output --> 3: fully-detailed per-batch console output
+train.init_security_pause = 0.0  # Short pause before erasing an existing run
+# Number of logged audio and spectrograms for a given epoch
+train.logged_samples_count = 4  # See update_dynamic_config_params()
+train.profiler_args = {'enabled': False, 'use_cuda': True, 'record_shapes': False,
+                       'profile_memory': False, 'with_stack': False}
+train.profiler_full_trace = False  # If True, runs only a few batches then exits - but saves a fully detailed trace.json
+train.profiler_1_GPU = False  # Profiling on only 1 GPU allow a much better understanding of trace.json
+
+
+evaluate = _Config()
+evaluate.epoch = -1  # Trained model to be loaded for post-training evaluation.
+
+
+# ---------------------------------------------------------------------------------------
+
+
+def update_dynamic_config_params():
+    """
+    Updates dynamic some global attributes of this config.py module.
+    This function should be called after any modification of this module's attributes.
+    """
+
+    # stack_spectrograms must be False for 1-note datasets - security check
+    model.stack_spectrograms = model.stack_spectrograms and (len(model.midi_notes) > 1)
+    # Artificially increased data size?
+    model.increased_dataset_size = (len(model.midi_notes) > 1) and not model.stack_spectrograms
+    model.concat_midi_to_z = (len(model.midi_notes) > 1) and not model.stack_spectrograms
+    # Mini-batch size can be smaller for the last mini-batches and/or during evaluation
+    model.input_tensor_size = (train.minibatch_size, 1 if not model.stack_spectrograms else len(model.midi_notes),
+                               model.spectrogram_size[0], model.spectrogram_size[1])
+
+    # Dynamic train hyper-params
+    train.early_stop_lr_threshold = train.initial_learning_rate * 1e-3
+    train.logged_samples_count = max(train.logged_samples_count, len(model.midi_notes))
+    # Train hyper-params (epochs counts) that should be increased when using a subset of the dataset
+    if model.dataset_synth_args[0] is not None:  # Limited Dexed algorithms?  TODO handle non-dexed synth
+        train.n_epochs = 700
+        train.lr_warmup_epochs = 10
+        train.scheduler_patience = 10
+        train.scheduler_cooldown = 10
+        train.beta_warmup_epochs = 40
+    # Train hyper-params (epochs counts) that should be reduced with artificially increased datasets
+    # Augmented  datasets introduce 6x more backprops <=> 6x more epochs. Patience and cooldown must however remain >= 2
+    if model.increased_dataset_size:  # Stacked spectrogram do not increase the dataset size (number of items)
+        N = len(model.midi_notes) - 1  # reduce a bit less that dataset's size increase
+        train.n_epochs = 1 + train.n_epochs // N
+        train.lr_warmup_epochs = 1 + train.lr_warmup_epochs // N
+        train.scheduler_patience = 1 + train.scheduler_patience // N
+        train.scheduler_cooldown = 1 + train.scheduler_cooldown // N
+        train.beta_warmup_epochs = 1 + train.beta_warmup_epochs // N
+
+    # Automatic model.synth string update - to summarize this info into 1 Tensorboard string hparam
+    if model.synth == "dexed":
+        if model.dataset_synth_args[0] is not None:  # Algorithms
+            model.synth_args_str = model.synth_args_str.replace("al*", "al" +
+                                                                '.'.join(
+                                                                    [str(alg) for alg in model.dataset_synth_args[0]]))
+        if model.dataset_synth_args[1] is not None:  # Operators
+            model.synth_args_str = model.synth_args_str.replace("_op*", "_op" +
+                                                                ''.join(
+                                                                    [str(op) for op in model.dataset_synth_args[1]]))
+        if model.dataset_labels is not None:  # Labels
+            model.synth_args_str = model.synth_args_str.replace("_lab*", '_' +
+                                                                '_'.join(
+                                                                    [label[0:4] for label in model.dataset_labels]))
+    else:
+        raise NotImplementedError("Unknown synth prefix for model.synth '{}'".format(model.synth))
+
+
+# Call this function again after modifications from the outside of this module
+update_dynamic_config_params()
+
